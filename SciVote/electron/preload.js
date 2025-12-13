6451924c800ccc28d3fb4073a40c3079a1cf7855
carlsagnip <sagnipcarlkenneth@gmail.com>
2025-12-13 13:54:16 +0800

    test

--- SciVote/electron/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  platform: process.platform,
-  versions: process.versions,
-  isElectron: true
-});
-
-console.log('SciVote preload script loaded successfully');
